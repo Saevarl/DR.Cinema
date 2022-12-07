ed6cdaa72ef4302e8d20068b6308d96f17410b2c
saevar bjornsson <saevarl3@gmail.com>
2022-12-07 19:46:38 +0000

    bla

--- src/screens/homescreen/index.js
+++ src/screens/homescreen/index.js
@@ -1,49 +1,7 @@
-<<<<<<< HEAD
-import { View, Text, TouchableHighlight } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { authenticate, selectToken } from '../../features/authenticationSlice';
-import { useSelector } from 'react-redux';
-import { USERNAME, PASSWORD } from '@env';
-import { useNavigation } from '@react-navigation/native';
-
-
-const HomeScreen = () => {
-    const dispatch = useDispatch()
-    const accessToken = useSelector(selectToken)
-    const [token, setToken] = useState('')
-
-    const navigation = useNavigation(); 
-   
-    useEffect(() => {
-        const credentials = {
-            username: `${USERNAME}`,
-            password: `${PASSWORD}`
-        }
-        console.log("CREDENTIALS", credentials)
-        dispatch(authenticate(credentials))
-
-            
-    }, [])
-
-  
-  
-    return (
-    <View>
-      <Text>TOKEN {token}</Text>
-      <TouchableHighlight
-            onPress={() => navigation.navigate("Upcoming")}>
-            <Text className="font-bold text-gray-500 p-2">Væntanlegar í bíó</Text>
-        </TouchableHighlight>
-    </View>
-    
-=======
-import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import {SafeAreaView, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Header from '../../components/header'
-import MovieCard from '../../components/movieCard'
-import Footer from '../../components/footer'
 import InCinemaList from '../../components/inCinemaList'
 
 const HomeScreen = () => {
@@ -72,7 +30,6 @@ const HomeScreen = () => {
         </ScrollView>
       
     </SafeAreaView>
->>>>>>> saevar
   )
 }
 
