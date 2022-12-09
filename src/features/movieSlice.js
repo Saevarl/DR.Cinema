2a70f8ca92de1cf4e80528d3df73db003b56c547
saevar bjornsson <saevarl3@gmail.com>
2022-12-09 12:30:17 +0000

    bla

--- src/features/movieSlice.js
+++ src/features/movieSlice.js
@@ -1,17 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { createAsyncThunk } from '@reduxjs/toolkit'
+import { useDispatch } from 'react-redux'
+import { USERNAME, PASSWORD } from '@env'
 
 const movieSlice = createSlice({
   name: 'movies',
   initialState: {
     movies: [],
     selectedMovie: null,
+    isLoading: false,
     },
 
   reducers: {
     setSelectedMovie: (state, action) => {
       console.log("SET SELECTED MOVIE", action.payload)
       state.selectedMovie = action.payload
+    },
+    
+    startLoadingMovies: (state) => {
+      state.isLoading = true
     }
 
     
@@ -20,37 +27,39 @@ const movieSlice = createSlice({
     extraReducers: (builder) => {
         builder
         .addCase(fetchMovies.fulfilled, (state, action) => {
-            console.log("MOVIES FETCHED")
+            console.log("FETCHED MOVIES")
             state.movies = action.payload
+            state.isLoading = false
         })
         .addCase(fetchMovies.rejected, (state, action) => {
-            console.log("REJECTED", action.error)
-
-        }
-        )
+            console.log("FETCH MOVIES REJECTED")
+            
+        })
+        
+        
 
     }
 })
 
 
   export const fetchMovies = createAsyncThunk(
-      'movies/fetchMovies',
-      async (token) => {
-          const res = await fetch(`https://api.kvikmyndir.is/movies?token=${token}`, {
-              method: 'GET',
-              
-              
-            })
-            
-          const data = await res.json()
-          return data
-      }
+    'movies/fetchMovies',
+    async (token) => {
+        const res = await fetch(`https://api.kvikmyndir.is/movies?token=${token}`, {
+            method: 'GET',
+          })
+          
+        const data = await res.json()
+        return data
+    }
   )
 
-  export const { setSelectedMovie } = movieSlice.actions
+  export const { setSelectedMovie, startLoadingMovies} = movieSlice.actions
 
   export const selectSelectedMovie = (state) => state.movies.selectedMovie
 
   export const selectMovies = (state) => state.movies.movies
+
+  export const selectIsLoading = (state) => state.movies.isLoading
   
   export default movieSlice.reducer
--- src/screens/homescreen/index.js
+++ src/screens/homescreen/index.js
@@ -1,11 +1,22 @@
-import {SafeAreaView, ScrollView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
+import {SafeAreaView, ScrollView, Text, View } from 'react-native'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import Header from '../../components/header'
 import InCinemaList from '../../components/inCinemaList'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchMovies, selectIsLoading, startLoadingMovies } from '../../features/movieSlice'
+import { authenticate, selectToken, selectTokenIsLoading, startLoadingToken } from '../../features/authenticationSlice'
+import { selectIsLoadingCinemas, startLoadingCinemas, fetchCinemas } from '../../features/cinemaSlice'
+import { USERNAME, PASSWORD } from '@env'
+import { ActivityIndicator } from '@react-native-material/core'
 
 const HomeScreen = () => {
   const navigation = useNavigation()
+  const dispatch = useDispatch()
+  const moviesAreLoading = useSelector(selectIsLoading)
+  const tokenIsLoading = useSelector(selectTokenIsLoading)
+  const cinemasAreLoading = useSelector(selectIsLoadingCinemas)
+  const accessToken = useSelector(selectToken)  
   
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -13,21 +24,50 @@ const HomeScreen = () => {
     })
 }, [])
 
+useEffect(() => {
+  const credentials = {
+    username: `${USERNAME}`,
+    password: `${PASSWORD}`
+}
+dispatch(startLoadingToken())
+dispatch(authenticate(credentials))
+}, [])
+
+useEffect(() => {
+if (accessToken) {
+    dispatch(startLoadingMovies())
+    dispatch(fetchMovies(accessToken))
+    dispatch(startLoadingCinemas())
+    dispatch(fetchCinemas(accessToken))
+  }
+}, [accessToken])
 
 
   return (
     <SafeAreaView 
               style={{backgroundColor: "#D3D0E3"}}
               className="flex-1">
-      <Header />
+      {
+        moviesAreLoading || tokenIsLoading || cinemasAreLoading
+        ?
+        <View className="justify-center items-center">
+          <ActivityIndicator
+            className="self-center"
+            size="large"
+            color="#00000f"
+          />
+        </View>
+        : 
+        <>
+          <Header />
       
-        <ScrollView >
-          <InCinemaList />
-          
-          
-         
-          
-        </ScrollView>
+          <ScrollView >
+            <InCinemaList />
+          </ScrollView>
+        </>
+        
+      }
+      
       
     </SafeAreaView>
   )
